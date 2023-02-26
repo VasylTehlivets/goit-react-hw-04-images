1ce1948582f9fea3900812a759a18f7964490dff
VasylTehlivets <vasyltehlivets@gmail.com>
2023-02-26 22:21:46 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-const meta = document.createElement('meta');
-meta.httpEquiv = 'Permissions-Policy';
-meta.content = 'interest-cohort=()';
-document.head.appendChild(meta);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
